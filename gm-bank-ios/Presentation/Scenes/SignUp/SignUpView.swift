331078149c4c8a806e958e37caa54d215130cf9d
Gilson Santos <gilsonsantos1998@gmail.com>
2022-01-25 17:14:13 -0300

    Create sign up view

--- gm-bank-ios/Presentation/Scenes/SignUp/SignUpView.swift
+++ gm-bank-ios/Presentation/Scenes/SignUp/SignUpView.swift
@@ -1,9 +1,68 @@
 import Cartography
 import UIKit
 
-protocol SignUpViewProtocol: UIView {}
+protocol SignUpViewDelegate: AnyObject {
+    func didTapOnButton()
+}
 
-final class SignUpView: UIView {
+protocol SignUpViewProtocol: UIView {
+    var delegate: SignUpViewDelegate? { get set }
+}
+
+final class SignUpView: UIView, SignUpViewProtocol {
+    
+    weak var delegate: SignUpViewDelegate?
+    
+    // MARK: - Properties
+    
+    private lazy var nameTextView: UITextField = {
+        let textView = UITextField()
+        textView.layer.cornerRadius = 6
+        textView.layer.borderWidth = 1.5
+        textView.backgroundColor = .white
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.textColor = .black
+        textView.font = .systemFont(ofSize: 16)
+        textView.autocorrectionType = .no
+        textView.placeholder = "Insira seu nome"
+        return textView
+    }()
+    
+    private lazy var emailTextView: UITextField = {
+        let textView = UITextField()
+        textView.layer.cornerRadius = 6
+        textView.layer.borderWidth = 1.5
+        textView.backgroundColor = .white
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.textColor = .black
+        textView.font = .systemFont(ofSize: 16)
+        textView.autocorrectionType = .no
+        textView.placeholder = "Insira seu email"
+        return textView
+    }()
+    
+    private lazy var passwordTextView: UITextField = {
+        let textView = UITextField()
+        textView.layer.cornerRadius = 6
+        textView.layer.borderWidth = 1.5
+        textView.backgroundColor = .white
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.textColor = .black
+        textView.font = .systemFont(ofSize: 16)
+        textView.autocorrectionType = .no
+        textView.placeholder = "Insira sua senha (precisa ter mais de 6 dígitos)"
+        return textView
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 6
+        button.backgroundColor = .white
+        button.setTitle("Cadastrar", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(didTapSubmitButton), for: .touchUpInside)
+        return button
+    }()
     
     // MARK: - Lifecycle
 
@@ -16,11 +75,63 @@ final class SignUpView: UIView {
         nil
     }
     
+    // MARK: - Helpers
+    
+    @objc func didTapSubmitButton() {
+        delegate?.didTapOnButton()
+    }
+    
     // MARK: - Constrains
 
     private func setup() {
-        self.backgroundColor = .red
+        self.backgroundColor = .black
+        setupConstrains()
+    }
+    
+    private func setupConstrains() {
+        constrainNameTextView()
+        constrainEmailTextView()
+        constrainPasswordTextView()
+        constrainButton()
+    }
+    
+    private func constrainNameTextView() {
+        addSubview(nameTextView)
+        constrain(nameTextView, self) { textView, view in
+            textView.leading == view.leading + 16
+            textView.top == view.top + 100
+            textView.trailing == view.trailing - 16
+            textView.height == 50
+        }
+    }
+    
+    private func constrainEmailTextView() {
+        addSubview(emailTextView)
+        constrain(emailTextView, nameTextView) { emailTextView, nameTextView in
+            emailTextView.top == nameTextView.bottom + 20
+            emailTextView.leading == nameTextView.leading
+            emailTextView.trailing == nameTextView.trailing
+            emailTextView.height == 50
+        }
+    }
+    
+    private func constrainPasswordTextView() {
+        addSubview(passwordTextView)
+        constrain(passwordTextView, emailTextView) { passwordTextView, emailTextView in
+            passwordTextView.top == emailTextView.bottom + 20
+            passwordTextView.leading == emailTextView.leading
+            passwordTextView.trailing == emailTextView.trailing
+            passwordTextView.height == 50
+        }
+    }
+    
+    private func constrainButton() {
+        addSubview(button)
+        constrain(button, passwordTextView) { button, textView in
+            button.top == textView.bottom + 40
+            button.leading == textView.leading + 70
+            button.trailing == textView.trailing - 70
+            button.height == 50
+        }
     }
 }
-
-extension SignUpView: SignUpViewProtocol {}
--- gm-bank-ios/Presentation/Scenes/SignUp/SignUpViewController.swift
+++ gm-bank-ios/Presentation/Scenes/SignUp/SignUpViewController.swift
@@ -23,12 +23,39 @@ final class SignUpController: UIViewController {
     required init?(coder: NSCoder) {
         nil
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     override func loadView() {
         view = customView
+        customView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.title = "Cadastro"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 26, weight: .bold)
+        ]
     }
 }
 
 extension SignUpController: SignUpDisplayLogic {
     
+}
+
+extension SignUpController: SignUpViewDelegate {
+    func didTapOnButton() {
+        print("BUTTON TAPPED")
+    }
 }
